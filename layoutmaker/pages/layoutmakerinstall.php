59e5fb234e9e9d9c925ba5b4c9fc17a34f5364ff
EzioisAwesome56 <jedijon567@gmail.com>
2017-02-09 16:35:49 -0500

    whoops, forgot to comment out the folder creation

--- layoutmaker/pages/layoutmakerinstall.php
+++ layoutmaker/pages/layoutmakerinstall.php
@@ -7,8 +7,8 @@ if(isset($_POST['action']) && $_POST['action'] == "Install")
 {
 	//Changed this to not to use the uploader since it's now a plugin. ~Dirbaio
 	//This isn't used anymore, instead the stylesheet is stored in the layout header ~EzioisAwesome56
-	if(!is_dir("layoutmaker"))
-		mkdir("layoutmaker/");
+	//if(!is_dir("layoutmaker"))
+		//mkdir("layoutmaker/");
 
 	//Honestly, I dont know why this code wasnt working with my devboard. Might have been rewritten URLs or folder perms.
 	//So ive just commented out the useless crap so it wont even run.
